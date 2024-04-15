22059583926baa115b6d13881cb4a6916aeae2e2
Eugene <fogaprod@gmail.com>
2024-04-15 16:26:21 +0400

    convert trades and prices to new ws trait

--- src/ws/trades.rs
+++ src/ws/trades.rs
@@ -1,94 +1,65 @@
 use std::sync::atomic::AtomicBool;
 
 use binance::websockets::agg_trade_stream;
-use binance::{websockets::WebSockets, ws_model::WebsocketEvent};
-use futures::sink::SinkExt;
-use iced::futures::FutureExt;
+use binance::ws_model::TradesEvent;
+use futures::channel::mpsc;
 use iced::subscription::{self, Subscription};
-use tokio::sync::mpsc;
 
 use crate::ws::WsEvent;
 
-use super::WsMessage;
+use super::{WsListener, WsMessage};
 
 #[derive(Debug, Clone)]
 pub(crate) enum Message {
     NewPair(String),
 }
 
-#[allow(clippy::large_enum_variant)]
-enum State<'a> {
-    Disconnected,
-    Connected(
-        WebSockets<'a, binance::ws_model::WebsocketEvent>,
-        mpsc::UnboundedReceiver<Message>,
-    ),
+struct TradesWs {
+    pair: String,
+    output: mpsc::Sender<WsMessage>,
 }
 
-pub fn connect(mut pair: String) -> Subscription<WsMessage> {
+impl TradesWs {
+    fn new(pair: String, output: mpsc::Sender<WsMessage>) -> Self {
+        Self { pair, output }
+    }
+}
+
+impl WsListener for TradesWs {
+    type Event = TradesEvent;
+    type Input = Message;
+    type Output = TradesEvent;
+
+    fn output(&mut self) -> &mut mpsc::Sender<WsMessage> {
+        &mut self.output
+    }
+
+    async fn endpoint(&self) -> String {
+        agg_trade_stream(&self.pair)
+    }
+
+    fn handle_event(&self, event: Self::Event) -> Self::Output {
+        event
+    }
+
+    fn handle_input(&mut self, input: Self::Input, keep_running: &mut AtomicBool) {
+        match input {
+            Message::NewPair(new_pair) => {
+                self.pair = new_pair;
+                keep_running.store(false, std::sync::atomic::Ordering::Relaxed);
+            }
+        };
+    }
+
+    fn message(&self, msg: WsEvent<Self::Input, Self::Output>) -> WsMessage {
+        WsMessage::Trade(msg)
+    }
+}
+
+pub fn connect(pair: String) -> Subscription<WsMessage> {
     struct Connect;
 
-    subscription::channel(
-        std::any::TypeId::of::<Connect>(),
-        100,
-        |mut output| async move {
-            let mut state = State::Disconnected;
-
-            let keep_running = AtomicBool::new(true);
-
-            loop {
-                match &mut state {
-                    State::Disconnected => {
-                        let mut output_clone = output.clone();
-                        let mut web_socket: WebSockets<'_, WebsocketEvent> =
-                            WebSockets::new(move |events: WebsocketEvent| {
-                                if let WebsocketEvent::AggTrade(tick_event) = events {
-                                    let _ = output_clone
-                                        .try_send(WsMessage::Trade(WsEvent::Message(*tick_event)));
-                                };
-
-                                Ok(())
-                            });
-
-                        let book_ticker: String = agg_trade_stream(&pair);
-                        match web_socket.connect(&book_ticker).await {
-                            Ok(()) => {
-                                let (sender, receiver) = mpsc::unbounded_channel();
-
-                                let _ = output
-                                    .send(WsMessage::Trade(WsEvent::Connected(sender)))
-                                    .await;
-                                state = State::Connected(web_socket, receiver);
-                            }
-                            Err(e) => {
-                                eprintln!("WebSocket connection error: {e:?}");
-                            }
-                        };
-                    }
-                    State::Connected(web_socket, input) => {
-                        futures::select! {
-                            ws_closed = web_socket.event_loop(&keep_running).fuse() => {
-                                if ws_closed.is_err() {
-                                    eprintln!("Trade stream error: {:?}", ws_closed.unwrap_err());
-                                }
-                            },
-                            input_message = input.recv().fuse() => {
-                                if let Some(input) = input_message {
-                                    match input {
-                                        Message::NewPair(new_pair) => {
-                                            pair = new_pair;
-                                        }
-                                    };
-                                }
-
-                            }
-                        };
-
-                        let _ = output.send(WsMessage::Trade(WsEvent::Disconnected)).await;
-                        state = State::Disconnected;
-                    }
-                }
-            }
-        },
-    )
+    subscription::channel(std::any::TypeId::of::<Connect>(), 100, |output| async {
+        TradesWs::new(pair, output).run().await
+    })
 }
